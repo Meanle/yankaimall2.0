9cf2b15c1a046056a463d30e63d0ce219cc65b9b
Meanlok <duanlele7@gmail.com>
2016-12-29 11:02:17 +0800

    安装数据库

---
+++ Apps/Runtime/Temp/2/0/2092c201129e3e1bda0a374bb1d528a5.php
@@ -0,0 +1,3 @@
+<?php
+//000000086400a:2:{i:0;a:4:{s:4:"adId";s:2:"19";s:6:"adName";s:1:"3";s:5:"adURL";s:0:"";s:6:"adFile";s:39:"Upload/adspic/2015-05/554c1785e590c.jpg";}i:1;a:4:{s:4:"adId";s:2:"20";s:6:"adName";s:1:"4";s:5:"adURL";s:0:"";s:6:"adFile";s:39:"Upload/adspic/2015-05/554c179b4a408.jpg";}}
+?>
